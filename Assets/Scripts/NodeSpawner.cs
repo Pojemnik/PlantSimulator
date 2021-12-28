7dce4126eb9814f25c7d97a3b9b8af4ecea10a6f
Pojemnik <andriej194@gmail.com>
2021-12-28 16:26:27 +0100

    Add inserting subnodes into created edges

--- Assets/Scripts/NodeSpawner.cs
+++ Assets/Scripts/NodeSpawner.cs
@@ -55,11 +55,11 @@ public class NodeSpawner : Singleton<NodeSpawner>
 
     public void PlaceNode()
     {
-        if(!nodePlacement)
+        if (!nodePlacement)
         {
             return;
         }
-        if(!CheckPlacementCorrectness())
+        if (!CheckPlacementCorrectness())
         {
             return;
         }
@@ -67,21 +67,25 @@ public class NodeSpawner : Singleton<NodeSpawner>
         PlantEdge newEdge = newEdgeObject.GetComponent<PlantEdge>();
         newEdge.Type = newEdgeType;
         newEdge.SetPositions(placementStartPosition, currentPosition);
-        if(placementStartNode is SubNode)
+        if (placementStartNode is SubNode)
         {
             SplitEdge();
         }
         PlantNode startNode = (PlantNode)placementStartNode;
         startNode.successors.Add(newEdge);
-        PlantNode NewEndNode = Instantiate(nodePrefab).GetComponent<PlantNode>();
-        NewEndNode.transform.position = LayersManager.Instance.GetPositionOnLayer(currentPosition, LayersManager.LayerNames.Nodes);
-        NewEndNode.edge = newEdge;
-        NewEndNode.GetComponent<CircleRenderer>().Calculate();
+        PlantNode newEndNode = Instantiate(nodePrefab).GetComponent<PlantNode>();
+        newEndNode.transform.position = LayersManager.Instance.GetPositionOnLayer(currentPosition, LayersManager.LayerNames.Nodes);
+        newEndNode.edge = newEdge;
+        newEndNode.GetComponent<CircleRenderer>().Calculate();
+        newEdge.begin = startNode;
+        newEdge.end = newEndNode;
+        PlaceSubnodes(newEdge);
         StopNodePlacement();
     }
 
     private void SplitEdge()
     {
+        //Subnodes should be transfered to new edges instead of being removed and created
         PlantNode createdNode = Instantiate(nodePrefab).GetComponent<PlantNode>();
         createdNode.transform.position = LayersManager.Instance.GetPositionOnLayer(placementStartNode.transform.position, LayersManager.LayerNames.Edges);
         PlantEdge edgeBefore = Instantiate(edgePrefab).GetComponent<PlantEdge>();
@@ -91,6 +95,7 @@ public class NodeSpawner : Singleton<NodeSpawner>
         Vector3 startPosition = LayersManager.Instance.GetPositionOnLayer(edgeBefore.begin.transform.position, LayersManager.LayerNames.Edges);
         Vector3 endPosition = LayersManager.Instance.GetPositionOnLayer(edgeBefore.end.transform.position, LayersManager.LayerNames.Edges);
         edgeBefore.SetPositions(startPosition, endPosition);
+        PlaceSubnodes(edgeBefore);
         PlantEdge edgeAfter = Instantiate(edgePrefab).GetComponent<PlantEdge>();
         edgeAfter.begin = createdNode;
         edgeAfter.Type = placementStartNode.edge.Type;
@@ -98,12 +103,38 @@ public class NodeSpawner : Singleton<NodeSpawner>
         startPosition = LayersManager.Instance.GetPositionOnLayer(edgeAfter.begin.transform.position, LayersManager.LayerNames.Edges);
         endPosition = LayersManager.Instance.GetPositionOnLayer(edgeAfter.end.transform.position, LayersManager.LayerNames.Edges);
         edgeAfter.SetPositions(startPosition, endPosition);
+        PlaceSubnodes(edgeAfter);
         createdNode.edge = edgeBefore;
         createdNode.successors = new List<PlantEdge>(new PlantEdge[] { edgeAfter });
+        foreach(SubNode subNode in placementStartNode.edge.subnodes)
+        {
+            Destroy(subNode.gameObject);
+        }
+        placementStartNode.edge.subnodes.Clear();
         Destroy(placementStartNode.edge.gameObject);
         Destroy(placementStartNode.gameObject);
         placementStartNode = createdNode;
         createdNode.GetComponent<CircleRenderer>().Calculate();
+    }
+
+    private void PlaceSubnodes(PlantEdge edge)
+    {
+        Vector3 startPos = edge.begin.transform.position;
+        Vector3 endPos = edge.end.transform.position;
+        Vector3 edgeVector = endPos - startPos;
+        float length = edgeVector.magnitude;
+        edgeVector = edgeVector.normalized;
+        length -= 0.8f;
+        int subnodesCount = (int)(length / 0.45f);
+        float distanceBetweenSubnodes = length / subnodesCount;
+        for (int i = 1; i <= subnodesCount; i++)
+        {
+            SubNode subNode = Instantiate(subnodePrefab).GetComponent<SubNode>();
+            subNode.transform.position = startPos + edgeVector * distanceBetweenSubnodes * i;
+            subNode.edge = edge;
+            subNode.GetComponent<CircleRenderer>().Calculate();
+            edge.subnodes.Add(subNode);
+        }
     }
 
     private bool CheckPlacementCorrectness()
@@ -113,7 +144,7 @@ public class NodeSpawner : Singleton<NodeSpawner>
             Debug.Log("Plant edge too short");
             return false;
         }
-        if(currentPosition.y < plantCore.transform.position.y && newEdgeType == PlantEdge.EdgeType.Stem)
+        if (currentPosition.y < plantCore.transform.position.y && newEdgeType == PlantEdge.EdgeType.Stem)
         {
             Debug.Log("Stem cannot be underground");
             return false;
@@ -130,7 +161,7 @@ public class NodeSpawner : Singleton<NodeSpawner>
     {
         temporaryEdge.edgeEnd.transform.position = position;
         currentPosition = position;
-        if(nodePlacement)
+        if (nodePlacement)
         {
             temporaryEdge.UpdateEdgePositions();
             temporaryEdge.PlacementCorrectness = CheckPlacementCorrectness();
